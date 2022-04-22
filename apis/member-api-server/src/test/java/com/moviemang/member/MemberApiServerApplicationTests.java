78f591adab9b724af72fb2089e72ae119eded409
beetrbgus <devbeetrb0214@gmail.com>
2022-04-23 00:18:50 +0900

    컨플릭트 해결

--- apis/member-api-server/src/test/java/com/moviemang/member/MemberApiServerApplicationTests.java
+++ apis/member-api-server/src/test/java/com/moviemang/member/MemberApiServerApplicationTests.java
@@ -5,10 +5,15 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import com.moviemang.datastore.domain.member.Member;
+import com.moviemang.datastore.repository.member.MemberRepository;
 import org.hibernate.validator.constraints.Length;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.moviemang.datastore.domain.member.Member;
+import com.moviemang.datastore.repository.member.MemberRepository;
 
 @SpringBootTest
 class MemberApiServerApplicationTests {
@@ -19,6 +24,12 @@ class MemberApiServerApplicationTests {
 	
     @Test
     void contextLoads() {
+    	Member member = Member.builder()
+    			.memberEmail("testUser1@gmail.com")
+    			.memberName("TestNick1")
+    			.memberPassword("gieiengien2")
+    			.build();
+    	
     	Member member = new Member();
     	member.setMemberName("TestNick1");
     	member.setMemberEmail("testUser1@gmail.com");
